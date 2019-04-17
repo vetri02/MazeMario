cfd84610f05c43374335f51867aae0904f326d16
Vetrichelvan Jeyapalpandy <vetri02@gmail.com>
2019-04-17 16:31:27 +0530

    Vanilla implementation

--- src/index.js
+++ src/index.js
@@ -25,21 +25,20 @@ window.mario = (function() {
       }
 
       // console.log(`${this.gridWidth} ${this.gridHeight}`);
-      this.buildTiles(
-        parseInt(this.gridWidth, 10),
-        parseInt(this.gridHeight, 10)
-      );
+      this.gridWidth = parseInt(this.gridWidth, 10);
+      this.gridHeight = parseInt(this.gridHeight, 10);
+      this.buildTiles(this.gridWidth, this.gridHeight);
     },
     buildMatrix: function(m, n) {
       let result = [];
       const marioLocation = Math.floor(Math.random() * n);
-      console.log(marioLocation);
+      // console.log(marioLocation);
       for (var i = 0; i < n; i++) {
         let fillLocation = Math.floor(Math.random() * m);
         let fillArray = new Array(m).fill(0);
         fillArray.fill(1, fillLocation, fillLocation + 1);
-        if (marioLocation - 1 === i) {
-          console.log(marioLocation);
+        if (marioLocation === i) {
+          // console.log(marioLocation);
           let marioFillLocation = Math.floor(Math.random() * m);
           if (marioFillLocation !== fillLocation) {
             fillArray.fill(2, marioFillLocation, marioFillLocation + 1);
@@ -69,16 +68,14 @@ window.mario = (function() {
         .join("<span class='clear'></span>");
 
       document.getElementById("app").innerHTML = markup;
-      this.keycodeEvents();
+      this.keycodeEvents(width, height);
     },
-    keycodeEvents: function() {
+    keycodeEvents: function(width, height) {
       const self = this;
-      const keyMap = {
-        37: "left",
-        38: "up",
-        39: "right",
-        40: "down"
-      };
+
+      let boardWidth = width;
+      let boardHeight = height;
+      console.log(boardWidth);
 
       function getIndexOfK(arr, k) {
         if (!arr) {
@@ -95,11 +92,53 @@ window.mario = (function() {
         return [];
       }
 
+      const keyMap = {
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
+      };
+
+      let marioPosition = getIndexOfK(self.board, 2);
+
       document.addEventListener("keydown", function(event) {
         if (event.keyCode === 37 || 38 || 39 || 40) {
+          // console.table(self.board);
+
           console.log(`Mario move: ${keyMap[event.keyCode]}`);
-          console.table(self.board);
-          console.log(getIndexOfK(self.board, 2));
+          console.log(marioPosition);
+          switch (event.keyCode) {
+            case 37:
+              marioPosition[1] =
+                marioPosition[1] > 0 ? marioPosition[1] - 1 : marioPosition[1];
+              console.log(`left: ${marioPosition}`);
+              break;
+
+            case 38:
+              marioPosition[0] =
+                marioPosition[0] > 0 ? marioPosition[0] - 1 : marioPosition[0];
+              console.log(`up: ${marioPosition}`);
+              break;
+
+            case 39:
+              console.log(boardWidth);
+              marioPosition[1] =
+                marioPosition[1] < boardWidth - 1
+                  ? marioPosition[1] + 1
+                  : marioPosition[1];
+              console.log(`right: ${marioPosition}`);
+              break;
+
+            case 40:
+              marioPosition[0] =
+                marioPosition[0] < boardHeight - 1
+                  ? marioPosition[0] + 1
+                  : marioPosition[0];
+              console.log(`down: ${marioPosition}`);
+              break;
+            default:
+              break;
+          }
           //document.querySelector(".mario").innerHTML = keyMap[event.keyCode];
         }
       });
